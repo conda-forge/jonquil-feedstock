ff8bf6e4688da3e48050380df2d675378ee687be
Sebastian Ehlert <sehlert@microsoft.com>
2026-06-09 17:56:34 +0200

    Add support for flang21

--- recipe/build_pkg.bat
+++ recipe/build_pkg.bat
@@ -1,7 +1,8 @@
 setlocal EnableExtensions EnableDelayedExpansion
 @echo on
 
-meson setup _build --prefix=%LIBRARY_PREFIX%
+if "%module_abi%"=="gfortran" (
+meson setup _build %MESON_ARGS% --prefix=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
 meson compile -C _build
@@ -12,3 +13,13 @@ if errorlevel 1 exit 1
 
 meson install -C _build --no-rebuild
 if errorlevel 1 exit 1
+) else (
+cmake -G Ninja -B _build %CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBUILD_SHARED_LIBS=ON -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON
+if errorlevel 1 exit 1
+
+cmake --build _build
+if errorlevel 1 exit 1
+
+cmake --install _build
+if errorlevel 1 exit 1
+)
